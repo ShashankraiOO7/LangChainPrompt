4b18b495665f52e015dd5d3c3ce3c2097305d1ee
ShashankraiOO7 <shashankrai020700@gmail.com>
2025-03-20 13:14:33 +0530

    After Using System,Human,AI messages to store History

--- ChatBot.py
+++ ChatBot.py
@@ -1,18 +1,22 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage,HumanMessage,AIMessage
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
 model=ChatGoogleGenerativeAI(model='gemini-1.5-pro')
-#Simple list to Store ChatHistory
+#Using list with Langachain Functionality to store Messages
 
-chat_history=[]
+chat_history=[
+    SystemMessage(content="You are Lawyer")
+    ]
 while True:
     user_input = input("User: ")
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
     if user_input == 'exit':
         break
     result=model.invoke(chat_history)
-    chat_history.append(result)
+    chat_history.append(AIMessage(content=result.content))
     print('AI: ',result.content)
+print(chat_history)
