57751397a584b12ac74c15db48cd98d2edc2c804
ShashankraiOO7 <shashankrai020700@gmail.com>
2025-03-20 12:26:00 +0530

    after implementation of simple Empty list to store chat History

--- ChatBot.py
+++ ChatBot.py
@@ -5,10 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 model=ChatGoogleGenerativeAI(model='gemini-1.5-pro')
-
+chat_history=[]
 while True:
     user_input = input("User: ")
+    chat_history.append(user_input)
     if user_input == 'exit':
         break
-    result=model.invoke(user_input)
+    result=model.invoke(chat_history)
+    chat_history.append(result)
     print('AI: ',result.content)
